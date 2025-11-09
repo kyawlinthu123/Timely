93113264b2acdf1820a403e669ad8e63a7d5d9d7
kyawlinthu123 <kyawlinthu655321@gmail.com>
2025-11-09 14:59:56 +0700

    axiosInstance modified

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001",
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:5001",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
